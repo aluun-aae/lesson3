4b4d37fee21375c0f071caf25de83873ff077955
aluun-aae <ergesh228822@gmail.com>
2021-03-16 00:39:38 +0600

    first commit

---
+++ lesson3.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool isTrue = true;
+
+  /// Booleon
+  // const str = "To";
+  const str = "do";
+  /* const нельзя
+  переписывать */
+  var c = 4;
+  int x = 8;
+  double a = 8.8;
+  String name = "Aluun";
+}
